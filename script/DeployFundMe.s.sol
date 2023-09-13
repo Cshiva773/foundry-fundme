0213198602ba3b136a1f9e4025b86f2d0508b952
shivansh <greatshiva773@gmail.com>
2023-09-13 14:13:32 +0530

    smart contract devlopment using brownie

---
+++ script/DeployFundMe.s.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.18;
+import {Script} from "forge-std/Script.sol";
+import {FundMe} from "../src/FundMe.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
+contract DeployFundMe is Script {
+    function run() external returns(FundMe){
+        HelperConfig help=new HelperConfig();
+        address priceFeed=help.activeNetworkConfig();
+        vm.startBroadcast();
+        FundMe fundme=new FundMe(priceFeed);
+        vm.stopBroadcast();
+        return fundme;
+    }
+}
